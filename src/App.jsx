c6316e86489c586417f7af518a414ef17fc244be
Anmol Rahujo <128142834+anmol-rahujo@users.noreply.github.com>
2024-09-09 12:24:27 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Anmol Rahujo",
+  title: "AI Specailist",
+  email: "anmolrahuloai@gmail.com",
+  gitHub: "anmol-rahujo",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "anmolrahujo",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "anmol
 };
 
 const primaryColor = "#4E567E";
